35f5c2227635318075db223c37020d8209dc9f7f
Serial Lab <slab@serial-lab.com>
2018-03-24 12:41:50 -0600

    Bump version: 1.0.10 → 1.0.11

--- EPCPyYes/__init__.py
+++ EPCPyYes/__init__.py
@@ -26,5 +26,5 @@
                                              "Y88P"
 '''
 
-__version__ = '1.0.10'
+__version__ = '1.0.11'
 __release__ = 'stable'
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = requirements + [
 
 setup(
     name='EPCPyYes',
-    version='1.0.10',
+    version='1.0.11',
     description="EPCIS Python module for quickly developing "
                 "EPCIS-enabled applications.",
     long_description=readme,
